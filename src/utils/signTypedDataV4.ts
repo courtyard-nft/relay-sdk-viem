f87fd2ac2fc4d658e6f905551747b4527348c1a2
Szabolcs Szentes <szmizorsz@gmail.com>
2024-03-19 18:14:11 +0100

    Some clean ups

--- src/utils/signTypedDataV4.ts
+++ src/utils/signTypedDataV4.ts
@@ -21,29 +21,14 @@ export const signTypedDataV4 = async (
     );
   }
 
-  return await client.signTypedData({
+  const signature = await client.signTypedData({
     account: client.account,
     domain: payload.domain,
     types: payload.types,
     message: payload.message,
     primaryType: payload.primaryType,
   });
-  // if (isLocalSigner(client)) {
-  //   return await client.signTypedData({
-  //     domain: payload.domain,
-  //     types: payload.types,
-  //     message: payload.message,
-  //   });
-  // }
-  // // Magic Connect accepts payload as an object
-  // if ((signerOrProvider.provider as unknown as { isMagic: boolean }).isMagic) {
-  //   return await signerOrProvider.send(SIGN_TYPED_DATA_V4, [address, payload]);
-  // }
-  // const signature = await client.send(SIGN_TYPED_DATA_V4, [
-  //   address,
-  //   JSON.stringify(payload),
-  // ]);
 
-  // // Support both versions of `eth_sign` responses
-  // return signature.replace(/00$/, "1b").replace(/01$/, "1c");
+  // Support both versions of `eth_sign` responses
+  return signature.replace(/00$/, "1b").replace(/01$/, "1c");
 };
